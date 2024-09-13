8c6613dbe89c0b64e1abcf720b3721a5adb6dc3c
Jumarnow <32901113+jumarnow@users.noreply.github.com>
2024-09-13 17:19:06 +0000

    secure_asset

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="{{asset('template/docs/css/main.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{secure_asset('template/docs/css/main.css')}}">
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Login - Omah Koding</title>
   </head>
