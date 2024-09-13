de4bc6389e08d7da433ea45735ab052fd6e1b382
Jumarnow <32901113+jumarnow@users.noreply.github.com>
2024-09-13 17:37:44 +0000

    pdf fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -75,7 +75,7 @@
         </li>
         <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-file-text"></i><span class="app-menu__label">Rekap Data</span><i class="treeview-indicator fa fa-angle-right"></i></a>
           <ul class="treeview-menu">
-            <li><a class="treeview-item" href="{{url('pegawai-pdf')}}"><i class="icon fa fa-circle-o"></i> Pegawai</a></li>
+            <li><a class="treeview-item" href="{{url('pegawai/pdf')}}"><i class="icon fa fa-circle-o"></i> Pegawai</a></li>
           </ul>
         </li>
       </ul>
